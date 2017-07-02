3dd67bd8e18a6fe3330c984727cec10940b32dfd
kultzak <adriano_fk@hotmail.com>
2017-07-02 01:24:20 -0300

    Initial commit

---
+++ seti_noticias/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for seti_noticias project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "seti_noticias.settings")
+
+application = get_wsgi_application()
