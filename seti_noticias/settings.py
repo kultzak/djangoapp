14739e44c6a7280213da8a4c88aceea2a2a595dc
kultzak <adriano_fk@hotmail.com>
2017-07-02 18:04:57 -0300

    alterações no settings

--- seti_noticias/settings.py
+++ seti_noticias/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'eqokr2=p!tbi(scpb)ttr*(en3t987e90+s*xez_@_0z=-6srf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
