9a7a11a58e9142fb2d113382832617f48e835124
Adriano Kultzak <adriano_fk@hotmail.com>
2017-07-04 13:20:34 -0300

    Update models.py
    solve unicode problem

--- polls/models.py
+++ polls/models.py
@@ -2,9 +2,11 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.utils.encoding import python_2_unicode_compatible
 
 # Create your models here.
 
+@python_2_unicode_compatible
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
